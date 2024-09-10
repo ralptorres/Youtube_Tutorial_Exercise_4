378d0c80fc845dcb01f6d52ee97d99e4dd120b91
ralptorres <ralp.torres@gmail.com>
2024-09-10 20:23:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let fname = ["Ralp", "Raymond", "Raiven"];
+let lname = ["Torres", "Villaflores", "Dotillos"];
+let age = [30, 25, 20];
+let id = 2;
+
+let output = `First Name: ${fname[id]}
+Last Name: ${lname[id]}
+Age: ${age[id]}`;
+
+alert(output);
